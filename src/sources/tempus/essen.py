78976ce1ec40cf126ee4d0096717da67696f6882
bergi <s.berke@netzkolchose.de>
2021-10-04 00:18:38 +0200

    rename essen kreis to stadt

--- src/sources/tempus/essen.py
+++ src/sources/tempus/essen.py
@@ -7,22 +7,22 @@ class TempusEssenBase(TempusBaseScraper):
 
 
 class TempusEssen(TempusEssenBase):
-    ID = "kressen"
-    NAME = "Kreis Essen Bürgeramt"
+    ID = "essen"
+    NAME = "Stadt Essen Bürgeramt"
     TEMPUS_ID = 21
     TEMPUS_EXTRA_PARAMS = ("anwendung=332", )
 
 
 class TempusEssenKfz(TempusEssenBase):
-    ID = "kressenkfz"
-    NAME = "Kreis Essen Kfz.-Zulassungsbehörde"
+    ID = "essenkfz"
+    NAME = "Stadt Essen Kfz.-Zulassungsbehörde"
     TEMPUS_ID = 21
     TEMPUS_EXTRA_PARAMS = ("anwendung=3352", )
 
 
 class TempusEssenFahr(TempusEssenBase):
-    ID = "kressenfa"
-    NAME = "Kreis Essen Fahrerlaubnisbehörde"
+    ID = "essenfa"
+    NAME = "Stadt Essen Fahrerlaubnisbehörde"
     TEMPUS_ID = 21
     TEMPUS_EXTRA_PARAMS = ("anwendung=3354", )
 
@@ -35,14 +35,14 @@ class TempusEssenBorbeckKfz(TempusEssenBase):
 
 
 class TempusEssenSozialEltern(TempusEssenBase):
-    ID = "kressensoze"
-    NAME = "Kreis Essen Elterngeld"
+    ID = "essensoze"
+    NAME = "Stadt Essen Elterngeld"
     TEMPUS_ID = 89
     TEMPUS_EXTRA_PARAMS = ("anwendung=1", )
 
 
 class TempusEssenSozialWohn(TempusEssenBase):
-    ID = "kressensozw"
-    NAME = "Kreis Essen Wohngeld"
+    ID = "essensozw"
+    NAME = "Stadt Essen Wohngeld"
     TEMPUS_ID = 89
     TEMPUS_EXTRA_PARAMS = ("anwendung=3", )
